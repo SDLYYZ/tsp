5028b5275f3b5396439dbbf2b9f0f04cf294a8e7
yzy1 <yzy-1@outlook.com>
2020-10-04 16:16:26 +0800

    First commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let searchBar;
+let timeText;
+
+function UpdateTime() {
+  timeText.innerHTML = (new Date()).format("hh:mm");
+}
+
+window.onload = () => {
+  searchBar = document.getElementById("search");
+  timeText = document.getElementById("time");
+  UpdateTime();
+  setInterval(UpdateTime, 1000);
+  document.onkeypress = (ev) => {
+    if (document.activeElement == document.body && !ev.ctrlKey && ev.key != "Enter") {
+      searchBar.placeholder = "Search...";
+      searchBar.disabled = false;
+      searchBar.focus();
+      if (ev.key == " ") {
+        searchBar.value = "";
+        return false;
+      }
+    }
+  }
+  searchBar.onkeypress = (ev) => {
+    if (ev.key == "Enter") {
+      QuoteSearch(searchBar.value);
+      searchBar.disabled = true;
+      searchBar.placeholder = "Searching...";
+      searchBar.value = "";
+    }
+  }
+}
